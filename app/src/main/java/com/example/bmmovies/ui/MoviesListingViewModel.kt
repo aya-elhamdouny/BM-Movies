9a91d712faee14a5811b1123d775f62919fc7b9d
aya khaled <52864131+aya-elhamdouny@users.noreply.github.com>
2024-03-06 03:58:43 +0200

    Update MoviesListingViewModel.kt

--- app/src/main/java/com/example/bmmovies/ui/MoviesListingViewModel.kt
+++ app/src/main/java/com/example/bmmovies/ui/MoviesListingViewModel.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
